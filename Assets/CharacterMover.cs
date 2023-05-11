a74bc53074b8789859c73e268529407c01a921db
Aleksei Golubev <>
2023-05-11 17:27:45 +0300

    Add gravity. Change control type

--- Assets/CharacterMover.cs
+++ Assets/CharacterMover.cs
@@ -4,9 +4,11 @@ public class CharacterMover : MonoBehaviour
 {
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKey(KeyCode.Space))
         {
             transform.position += Vector3.right * 10;
         }
+
+        transform.position -= Vector3.up * (9.8f * Time.deltaTime);
     }
 }
